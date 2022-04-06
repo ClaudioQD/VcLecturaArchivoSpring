21df065eee2adbf867d1794a20644c7aa7c96990
ClaudioQD <claudio.quijada.huawei@gmail.com>
2022-04-06 02:59:22 -0400

    proyecto completo

---
+++ src/main/java/com/rMvcLecturaArchivo/SpringMvcConLecturaTxtApplication.java
@@ -0,0 +1,13 @@
+package com.rMvcLecturaArchivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcConLecturaTxtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcConLecturaTxtApplication.class, args);
+	}
+
+}
